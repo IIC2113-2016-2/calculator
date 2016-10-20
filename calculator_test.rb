75c9fa4b88e90d37107e5081c1bf1adfbaa91a41
Antonio Fontaine <afontaine@uc.cl>
2016-10-20 13:05:44 -0300

    tests for multiply divide and type string

--- calculator_test.rb
+++ calculator_test.rb
@@ -11,4 +11,60 @@ class TestCalculator < Minitest::Test
     assert_equal 0, @calculator.calculate_answer("add", 0, 0)
   end
 
+  def test_multiply
+    operation = 'multiply'
+    assert_equal 8, @calculator.calculate_answer(operation, 4, 2)
+    assert_equal -8, @calculator.calculate_answer(operation, 4, -2)
+    assert_equal 8, @calculator.calculate_answer(operation, -4, -2)
+  end
+
+  def test_multiply_by_zero
+    operation = 'multiply'
+    assert_equal 0, @calculator.calculate_answer(operation, 4, 0)
+    assert_equal 0, @calculator.calculate_answer(operation, 0, -2)
+    assert_equal 0, @calculator.calculate_answer(operation, 0, 1)
+    assert_equal 0, @calculator.calculate_answer(operation, 0, 0)
+  end
+
+  def test_multiply_by_one
+    operation = 'multiply'
+    assert_equal 4, @calculator.calculate_answer(operation, 4, 1)
+    assert_equal -2, @calculator.calculate_answer(operation, 1, -2)
+    assert_equal 1, @calculator.calculate_answer(operation, 1, 1)
+    assert_equal 0, @calculator.calculate_answer(operation, 1, 0)
+  end
+
+  def test_divide
+    operation = 'divide'
+    assert_equal 2, @calculator.calculate_answer(operation, 4, 2)
+    assert_equal -2, @calculator.calculate_answer(operation, 4, -2)
+    assert_equal 0, @calculator.calculate_answer(operation, -2, -4)
+  end
+
+  def test_divide_by_zero
+    operation = 'divide'
+    assert_equal 'error', @calculator.calculate_answer(operation, 4, 0)
+    assert_equal 'error', @calculator.calculate_answer(operation, -4, 0)
+  end
+
+  def test_divide_zero
+    operation = 'divide'
+    assert_equal 0, @calculator.calculate_answer(operation, 0, -2)
+    assert_equal 0, @calculator.calculate_answer(operation, 0, 4)
+  end
+
+  def test_divide_by_one
+    operation = 'divide'
+    assert_equal 4, @calculator.calculate_answer(operation, 4, 1)
+    assert_equal -2, @calculator.calculate_answer(operation, -2, 1)
+    assert_equal 1, @calculator.calculate_answer(operation, 1, 1)
+    assert_equal 0, @calculator.calculate_answer(operation, 0, 1)
+  end
+
+
+  def test_by_string
+    operation = 'add'
+    assert_equal 'error', @calculator.calculate_answer(operation, 'hola', 1)
+  end
+
 end
