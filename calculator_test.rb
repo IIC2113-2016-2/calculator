a514ae59b5fc5e9c5f89e8400e48065e586198f4
TheDG <dsinay@uc.cl>
2016-10-20 12:54:58 -0300

    Substraction Test

--- calculator_test.rb
+++ calculator_test.rb
@@ -2,13 +2,29 @@ require 'minitest/autorun'
 require './calculator'
 
 class TestCalculator < Minitest::Test
-
   def setup
     @calculator = Calculator.new
   end
 
-  def test_add_basic
-    assert_equal 0, @calculator.calculate_answer("add", 0, 0)
+  def test_substractition
+    assert_equal 0, @calculator.calculate_answer('add', 0, 0)
+    assert_equal 1, @calculator.calculate_answer('add', 1, 0)
+    assert_equal 1, @calculator.calculate_answer('add', 0, 1)
+    assert_equal -1, @calculator.calculate_answer('add', -1, 0)
+    assert_equal -1, @calculator.calculate_answer('add', 0, -1)
+    assert_equal 0, @calculator.calculate_answer('add', -1, 1)
+    assert_equal 2, @calculator.calculate_answer('add', 1, 1)
+    assert_equal -2, @calculator.calculate_answer('add', -1, -1)
   end
 
+  def test_subtraction
+    assert_equal 0, @calculator.calculate_answer('subtract', 0, 0)
+    assert_equal 1, @calculator.calculate_answer('subtract', 1, 0)
+    assert_equal -1, @calculator.calculate_answer('subtract', 0, 1)
+    assert_equal -1, @calculator.calculate_answer('subtract', -1, 0)
+    assert_equal 1, @calculator.calculate_answer('subtract', 0, -1)
+    assert_equal -2, @calculator.calculate_answer('subtract', -1, 1)
+    assert_equal 0, @calculator.calculate_answer('subtract', 1, 1)
+    assert_equal 0, @calculator.calculate_answer('subtract', -1, -1)
+  end
 end
